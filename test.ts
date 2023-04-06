6414b73c336f2f970d1f505590b5b024cbd7ebfc
Yousef Gamal <youseffgamal123@gmail.com>
2023-04-07 01:33:05 +0200

    removed unneccessary test code

--- test.ts
+++ test.ts
@@ -7,76 +7,13 @@ const moodle = MoodleApi({
   token: '3752087c24677b2960cc03b821bd27cd',
 });
 
-// moodle.core.webservice.getSiteInfo().then((response: any) => {
-//   console.log(response);
-// });
-
 // moodle.core.course
 //   .getCoursesByField({ params: { field: 'id', value: '493' } })
 //   .then((response) => console.log(response));
 
-// moodle.core.course
-//   .getContents({ params: { courseid: '494' } })
-//   .then((res) => console.log(res));
-
-// moodle.mod.quiz
-//   .getAttemptData({ params: { attemptid: 3658573, page: 0 } })
-//   .then((res) => console.log(res));
-
 // moodle.enrol.self
 //   .enrolUser({ params: { courseid: 577 } })
 //   .then((res) => console.log(res));
-
-// moodle.mod.quiz.startAttempt({ params: { quizid: 1361 } }).then((res) => {
-//   console.log(res);
-// });
-
-// moodle.mod.quiz
-//   .saveAttempt({
-//     attemptid: 3659233,
-//     data: {
-//       data: [
-//         {
-//           name: 'slots',
-//           value: '2',
-//         },
-//         {
-//           name: 'q3662334:2_:sequencecheck',
-//           value: '1',
-//         },
-//         {
-//           name: 'q3662334:2_answer',
-//           value: '1',
-//         },
-//         {
-//           name: 'slots',
-//           value: '3',
-//         },
-//         {
-//           name: 'q3662334:3_:sequencecheck',
-//           value: '1',
-//         },
-//         {
-//           name: 'q3662334:3_answer',
-//           value: '2',
-//         },
-//       ],
-//     },
-//   })
-//   .then((result) => console.log(result));
-
-// moodle.core.course.getCategories().then((cats) => console.log(cats[0].name));
-// moodle.mod.forum
-//   .getDiscussionPosts({ discussionid: 70 })
-//   .then((res) => console.log(res));
-
-// moodle.mod.forum.getForumDiscussions({ forumid: 28 }).then((res) => {
-//   console.log(res);
-// });
-
-// moodle.mod.quiz
-//   .getUserAttempts({ quizid: 1360, status: 'all' })
-//   .then((res) => console.log(res.attempts.at(-1)!.state));
 
 // moodle.core.course.getCourseModule({ cmid: 5325 }).then((res) => {
 //   moodle.mod.quiz
@@ -98,8 +35,3 @@ moodle.mod.quiz
 // moodle.core.course
 //   .getCourseModule({ cmid: 5325 })
 //   .then((res) => console.log(res));
-
-/* data: [
-    {slots: 1, 'q3662334:2_:sequencecheck': 1, }
-
-  ]*/
