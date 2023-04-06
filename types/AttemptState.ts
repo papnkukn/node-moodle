541c2d839f438e365552eef3f445c7c4d25e83bd
Yousef Gamal <youseffgamal123@gmail.com>
2023-04-07 01:19:28 +0200

    update types

--- types/AttemptState.ts
+++ types/AttemptState.ts
@@ -1,5 +1,7 @@
 enum AttemptState {
   Finished = 'finished',
   InProgress = 'inprogress',
+  Overdue = 'overdue',
+  Abandoned = 'abandoned',
 }
 export default AttemptState;
--- types/QuestionStatus.ts
+++ types/QuestionStatus.ts
@@ -1,5 +1,8 @@
 enum QuestionStatus {
   AnswerSaved = 'Answer saved',
   NotYetAnswered = 'Not yet answered',
+  NotAnswered = 'Not answered',
+  Correct = 'Correct',
+  Incorrect = 'Incorrect',
 }
 export default QuestionStatus;
