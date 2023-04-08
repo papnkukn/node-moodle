d87e587633ce31c48271838d928e53c7cd0fa03e
Yousef Gamal <youseffgamal123@gmail.com>
2023-04-08 13:07:19 +0200

    test code

--- test.ts
+++ test.ts
@@ -1,10 +1,9 @@
 import MoodleApi, { MoodleClient } from '.';
-import MoodleAttemptUpdate from './classes/MoodleAttemptUpdate';
-import IMoodleAttempt from './interfaces/IMoodleAttempt';
-import AttemptStates from './types/AttemptStates';
-import QuizAccessRules from './types/QuizAccessRules';
 
 const moodle = MoodleApi({
   baseUrl: 'http://aunonline.aun.edu.eg/med-ns/',
   token: '3752087c24677b2960cc03b821bd27cd',
 });
+
+moodle.core.webservice.getSiteInfo();
+moodle.core.course.getCourseModule({ cmid: 5620 });
